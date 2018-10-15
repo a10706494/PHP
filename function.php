2df269b8ff3091f8efe32b5dd509a7351334ca20
a10706494 <34257542+a10706494@users.noreply.github.com>
2018-10-15 09:39:22 +0800

    Update function.php

--- function.php
+++ function.php
@@ -771,4 +771,29 @@ function arrangement($n, $m) {
 function combination($n, $m) {
     return arrangement($n, $m)/factorial($m);
 }
+/**
+ * 模拟post进行url请求,类型为json
+ * @param string $url
+ * @param string $param
+ */
+function request_post($url = '', $param = '') {
+    if (empty($url) || empty($param)) {
+        return false;
+    }
+
+    $ch = curl_init();//初始化curl
+    curl_setopt($ch, CURLOPT_URL,$url);//抓取指定网页
+    curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+    curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($param))
+    );
+    $data = curl_exec($ch);//运行curl
+    curl_close($ch);
+
+    return $data;
+}
 
